01666c59c7d8123c87155b0ad266779825ae4650
certafonso <44088727+certafonso@users.noreply.github.com>
2020-07-22 16:03:09 +0100

    WikiAgainstHumanity Completed

--- WikiAgainstHumanity.py
+++ WikiAgainstHumanity.py
@@ -12,8 +12,7 @@ class Game():
 		self.Players = players
 		self.GameStage = 0
 		self.Ready = []
-		# self.submissions = []
-		self.submissions = ["test1","test2","test3"]
+		self.submissions = []
 		self.VotingOrder = []
 		self.Votes = []
 		self.article = ""
@@ -29,26 +28,24 @@ class Game():
 				if command[0] == "-submit":
 					await self.Receive_Submit(message.author, " ".join(command[1:]))
 
-			elif self.GameStage == 1:				
+			elif self.GameStage == 1:	# game in stage 0, receiving submissions
 				if command[0] == "-vote":
 					await self.Receive_Vote(message.author, command[1])
 
-		# else:
-		# 	if self.GameStage == 1:     # game in stage 1 - guessing the articles
-		# 		if command[0] == "-guess" and message.author == self.Players[self.Guesser]:
-		# 			await self.Guess(command[1])
-		# 	elif self.GameStage == 2:   # game in stage 2 - new round or quit
-		# 		if command[0] == "-play" and message.author == self.Players[0]:
-		# 			await self.setup()
+		else:
+			if self.GameStage == 2:   # game in stage 2 - new round or quit
+				if command[0] == "-play" and message.author == self.Players[0]:
+					await self.setup()
 
 	async def setup(self):
 		"""Starts the round"""
 
 		self.GameStage = 0
-		self.Ready = [False for j in range(0, len(self.Players))]	# resets ready
+		self.Ready = [False for j in range(0, len(self.Players))]		# resets ready
+		self.submissions = ["" for j in range(0, len(self.Players))]	# resets submissions
 		self.Get_Article()	# selects an article
 
-		await self.Channel.send("Sending article to everyone...")
+		await self.Channel.send("Sending the article to everyone...")
 
 		for i in range(0, len(self.Players)):	# sends the message to everyone
 			dm = self.Players[i].dm_channel
@@ -56,14 +53,12 @@ class Game():
 				await self.Players[i].create_dm()
 				dm = self.Players[i].dm_channel
 
-			await dm.send(f"""{self.article_text}\n
-			Type `-submit` followed by the text you want to submit
-			""")
-
 			try:
-				self.submissions[i] = ""
-			except IndexError:
-				self.submissions.append("")
+				await dm.send(f"""{self.article_text}\n
+				Type `-submit` followed by the text you want to submit
+				""")
+			except discord.errors.HTTPException:	# probably the article was too long, let's try again
+				await self.Channel.send("An error ocurred, sending another article...")
 
 	def Get_Article(self):
 		""" Gets a new article and removes the title """
@@ -130,22 +125,19 @@ class Game():
 
 		# Display the submissions
 
-		cnt = 1
+		cnt = 0
 
 		message = self.article_text + "\n"
 
 		order = list(range(0, len(self.submissions)))
 		random.shuffle(order)	#shuffle the articles
 
+		self.VotingOrder = [0 for j in range(0, len(self.submissions))]	# resets voting order
+
 		for i in order:	# display the articles
 			message += f"\n{cnt} - {self.submissions[i]}"
+			self.VotingOrder[i] = cnt
 			cnt += 1
-
-			try:
-				self.VotingOrder[i] = cnt
-			except IndexError:
-				self.VotingOrder = [0 for j in range(0, len(self.submissions))]	# fills the voting order list
-				self.VotingOrder[i] = cnt
 
 		message += "\n\nTo vote dm me `-vote [option]`. Obviously you can't vote for yourself."
 
@@ -164,12 +156,10 @@ class Game():
 		try:
 			vote = int(vote)
 		except:
-			await dm.send("An error ocurred! Please try again.")
+			await dm.send("An error ocurred! Check if you inserted a number.")
 			return
 
 		real_vote = self.VotingOrder[vote]
-
-		print(self.Players.index(player))
 
 		if real_vote != self.Players.index(player):	# player voted on another person
 			self.Votes[real_vote] += 1			
@@ -177,8 +167,30 @@ class Game():
 
 			n_ready = self.CheckReady()
 
-			await self.Channel.send(f"{player.mention} is ready ({n_ready}/{len(self.Ready)})")
+			await self.Channel.send(f"{player.mention} voted ({n_ready}/{len(self.Ready)})")
 			await dm.send(f"Great! Now wait for everyone to be ready")
+
+			n_ready = self.CheckReady()
+
+			if n_ready == len(self.Players):	# everyone voted
+				await self.Count_Votes()
 
 		else:
 			await dm.send("You can't vote in yourself")
+
+	async def Count_Votes(self):
+		""" Counts the votes and displays the result """
+
+		self.GameStage = 2
+
+		message = "And the results are... \n"
+
+		for i in range(0, len(self.Players)):
+			message += f"\"{self.submissions[i]}\" {self.Players[i].mention} - {self.Votes[i]} votes \n"
+
+		message += f"The winner is {self.Players[self.Votes.index(max(self.Votes))].mention} \n"
+
+		# play again?
+		message += f"Type `-play` to play again or `-quit` to go to the main menu."
+
+		await self.Channel.send(message)
